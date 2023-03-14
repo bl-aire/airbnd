77e1e4dff3ed77db010a4951026982f111de0476
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-14 14:17:06 +0100

    Format code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,10 @@ import group from '../assets/Group.png'
 
 export default function Hero() {
     return(
-        <div className='hero'>
-            <div className='hero_container'>
-                <img src={group} className="hero_image"/>
-            </div>
-            <div className='text'>
-                <h1 className='hero_title'>Online Experiences</h1>
-                <p className='hero_text'>Join unique interactive activities led by one-of-a-kind hosts all without leaving your home.</p>
-            </div>
-        </div>
+        <section className='hero'>
+            <img src={group} className="hero_image"/>
+            <h1 className='hero_title'>Online Experiences</h1>
+            <p className='hero_text'>Join unique interactive activities led by one-of-a-kind hosts all without leaving your home.</p>
+        </section>
     )
 }

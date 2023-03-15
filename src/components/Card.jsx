7cc160428a5a41a985b3b0a14d18f8cb957a194f
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-03-15 18:25:51 +0100

    Style card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,18 @@ import smile from '../assets/smile.png'
 
 export default function Card() {
     return (
-        <section>
-            <div className='card'>
-                <div className='image_bg'>
-                    <button className='sold-out'>Sold out</button>
-                </div>
-                <span className='rating'>
-                    <img src={star} alt="star icon" className='star'/>
-                    <p>5.0 <span>.USA</span></p>
-                </span>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><b>From $136</b> / person</p>
+        <div className='card'>
+            <div className='image_bg'>
+                <button className='sold-out'>Sold out</button>
             </div>
-        </section>
-
+            <span className='rating'>
+                <img src={star} alt="star icon" className='star' />
+                <span>5.0</span>
+                <span>(6) • </span>
+                <span>USA</span>
+            </span>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><b>From $136</b> / person</p>
+        </div>
     )
 }
